481e5435c17d966bf372ab01110a29be09f3f75a
AdamChen <mnre2003@yahoo.com.tw>
2019-06-06 11:53:25 +0800

    refine checkbox layout

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/arch/persistence/room/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/arch/persistence/room/R.java
@@ -86,23 +86,23 @@ public final class R {
         public static final int async = 0x7f080020;
         public static final int blocking = 0x7f080024;
         public static final int chronometer = 0x7f08003c;
-        public static final int forever = 0x7f08005e;
-        public static final int icon = 0x7f080066;
-        public static final int icon_group = 0x7f080067;
-        public static final int info = 0x7f08006a;
-        public static final int italic = 0x7f08006c;
-        public static final int line1 = 0x7f080071;
-        public static final int line3 = 0x7f080072;
-        public static final int normal = 0x7f080080;
-        public static final int notification_background = 0x7f080081;
-        public static final int notification_main_column = 0x7f080082;
-        public static final int notification_main_column_container = 0x7f080083;
-        public static final int right_icon = 0x7f080090;
-        public static final int right_side = 0x7f080091;
-        public static final int text = 0x7f0800c1;
-        public static final int text2 = 0x7f0800c2;
-        public static final int time = 0x7f0800cf;
-        public static final int title = 0x7f0800d0;
+        public static final int forever = 0x7f08005f;
+        public static final int icon = 0x7f080067;
+        public static final int icon_group = 0x7f080068;
+        public static final int info = 0x7f08006b;
+        public static final int italic = 0x7f08006d;
+        public static final int line1 = 0x7f080072;
+        public static final int line3 = 0x7f080073;
+        public static final int normal = 0x7f080081;
+        public static final int notification_background = 0x7f080082;
+        public static final int notification_main_column = 0x7f080083;
+        public static final int notification_main_column_container = 0x7f080084;
+        public static final int right_icon = 0x7f080091;
+        public static final int right_side = 0x7f080092;
+        public static final int text = 0x7f0800c2;
+        public static final int text2 = 0x7f0800c3;
+        public static final int time = 0x7f0800d0;
+        public static final int title = 0x7f0800d1;
     }
     public static final class integer {
         private integer() {}
--- app/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/android/support/constraint/R.java
@@ -75,18 +75,18 @@ public final class R {
 
         public static final int bottom = 0x7f080025;
         public static final int end = 0x7f080052;
-        public static final int gone = 0x7f080061;
-        public static final int invisible = 0x7f08006b;
-        public static final int left = 0x7f080070;
-        public static final int packed = 0x7f080085;
-        public static final int parent = 0x7f080087;
-        public static final int percent = 0x7f08008a;
-        public static final int right = 0x7f08008f;
-        public static final int spread = 0x7f0800b2;
-        public static final int spread_inside = 0x7f0800b3;
-        public static final int start = 0x7f0800b8;
-        public static final int top = 0x7f0800d3;
-        public static final int wrap = 0x7f0800e2;
+        public static final int gone = 0x7f080062;
+        public static final int invisible = 0x7f08006c;
+        public static final int left = 0x7f080071;
+        public static final int packed = 0x7f080086;
+        public static final int parent = 0x7f080088;
+        public static final int percent = 0x7f08008b;
+        public static final int right = 0x7f080090;
+        public static final int spread = 0x7f0800b3;
+        public static final int spread_inside = 0x7f0800b4;
+        public static final int start = 0x7f0800b9;
+        public static final int top = 0x7f0800d4;
+        public static final int wrap = 0x7f0800e3;
     }
     public static final class styleable {
         private styleable() {}
